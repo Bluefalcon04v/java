f13f13a1e3191586f803e0cf9d084027682727df
Amit Sah <amitsah04v@gmail.com>
2025-11-08 13:18:13 +0530

    added the file post-pre increment and switch cases

---
+++ PostPreIncrement.java
@@ -0,0 +1,10 @@
+public class PostPreIncrement{
+    public static void main(String args[]){
+        byte a = 2;
+        System.out.println(a++); // first print then increment
+
+        byte b = 2;
+        System.out.println(++b); // first increment then print
+
+    }
+}
---
+++ Switch.java
@@ -0,0 +1,15 @@
+public class Switch{
+    public static void main(String args[]){
+        int n = 5;
+
+        switch(n){
+            case 1: System.out.println("monday");
+            break;
+            case 2: System.out.println("tuesday");
+            break;
+            case 3: System.out.println("wednesday");
+            break;
+            default: System.out.println("enter the valid number");
+        }
+    }
+}
