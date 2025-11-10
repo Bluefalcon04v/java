2f3d84f77ea7c1a548ba023f3993c5e625384e15
Amit Sah <amitsah04v@gmail.com>
2025-11-10 19:00:14 +0530

    wip

--- CreatingObj.java
+++ CreatingObj.java
@@ -1,9 +1,13 @@
 // creating a class
 class Calculator { 
-
+    int a;
+    public int  sub(int a, int b){
+        return  b-a;
+    }
     // writing methods
     public int add(int num1, int num2){
         int res  = num1 + num2;
+        System.out.println(a);
         return res;
     }
 }
@@ -16,7 +20,7 @@ public class CreatingObj {
 
         Calculator calc = new Calculator(); // defining a new Object
         int result =  calc.add(a,b); 
-
+        System.err.println(calc.sub(a, b));
         System.out.println(result);
     }
 }
