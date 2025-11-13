59b7685cc26f22bb5bb28ff199624c7e7cd4a62e
Amit Sah <amitsah04v@gmail.com>
2025-11-13 18:38:35 +0530

    wip

---
+++ Array.java
@@ -0,0 +1,21 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = new int[2];
+
+        int arr2[] = new int[3];
+        arr2[0] = 4;
+        arr2[1] = 1;
+        arr2[2] = 3;
+
+
+        // for(int i = 0; i< arr.length; i++){
+        //     System.err.println(arr2[i]);
+        // }
+
+// --------------------------------- Multi dimensional array
+
+    int multi[][][] = new int[3][4][5]; 
+    System.out.println(multi[1][2].length);
+
+    }
+}
